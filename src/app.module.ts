530628cb00fdaf6aa6d6f7539ef020b6e717244a
khjeong <wjdrhkd456@gmail.com>
2023-11-23 17:54:28 +0900

    [Ted] mongoose 제거
    1. npm package 제거
    - mongoose
    - @nestjs/mongoose
    
    2. app.module.ts 내 mongooseModule 제거

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,32 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsModule } from './cats/cats.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { ormConfig } from './orm.config';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import config from './config/config';
-import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
+import { RoleModule } from './role/role.module';
+import { CatModule } from './cat/cat.module';
 
 @Module({
     imports: [
-        MongooseModule.forRootAsync({
-            imports: [ConfigModule],
-            inject: [ConfigService],
-            useFactory: async (configService: ConfigService) => ({
-                uri: configService.get<string>('MONGO_URI')
-            })
-        }),
         ConfigModule.forRoot({
             load: [config],
             isGlobal: true,
         }),
         TypeOrmModule.forRootAsync({ useFactory: ormConfig }),
-        CatsModule,
         AuthModule,
-        UserModule
+        UserModule,
+        RoleModule,
+        CatModule
     ],
     controllers: [AppController],
     providers: [AppService],
